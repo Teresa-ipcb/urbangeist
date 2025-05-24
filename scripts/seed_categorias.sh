ec6a5a766bebc58518a2fecf12084a2fb4455a06
Teresa-ipcb <c.teresa@ipcbcampus.pt>
2025-05-24 18:25:52 +0100

    Add files via upload

--- scripts/seed_categorias.sh
+++ scripts/seed_categorias.sh
@@ -13,6 +13,7 @@ COSMOSDB_CONN_STRING=$(az cosmosdb keys list --type connection-strings \
 
 # Exportar para usar no process.env
 export COSMOSDB_CONN_STRING
+
 npm install mongodb@3.7 > /dev/null 2>&1
 export NODE_PATH=./node_modules
 
@@ -23,7 +24,7 @@ const { MongoClient } = require("mongodb");
 
 (async () => {
   const uri = process.env.COSMOSDB_CONN_STRING;
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  const client = new MongoClient(uri);
 
   await client.connect();
   const db = client.db("urbangeist");
