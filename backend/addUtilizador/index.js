f3ed15933dcc17a1bcaefceea64a6b8fb799a87e
Teresa-ipcb <c.teresa@ipcbcampus.pt>
2025-05-28 10:40:39 +0100

    Update index.js

--- backend/addUtilizador/index.js
+++ backend/addUtilizador/index.js
@@ -23,26 +23,19 @@ module.exports = async function (context, req) {
         await collection.insertOne({ nome, email, password });
 
         // efetuar login
-        const loginRes = await fetch(`${process.env.BASE_URL}/api/login`, {
+        const loginRes = await fetch(`https://urbangeist-function.azurewebsites.net/api/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Origin': origin
             },
             body: JSON.stringify({ email, password }),
-            credentials: 'include'
         });
 
         const responseBody = await loginRes.json();
 
-        const rawSetCookie = loginRes.headers.getSetCookie?.() || loginRes.headers.get('set-cookie');
-
         context.res = {
             status: loginRes.status,
-            headers: {
-                ...headers,
-                'Set-Cookie': rawSetCookie
-            },
             body: responseBody
         };
     } catch (err) {
